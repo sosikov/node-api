7da9873bc85a9aa35a9a83502a0bf9449a931a33
sosikov <n.sosikov@gmail.com>
2019-10-09 15:45:10 +0300

    add some middlewares

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const dotEnv = require('dotenv');
+const cors = require('cors');
 
 dotEnv.config();
 
 const app = express();
 
-app.get('/', (req, res, next) => {
+// cors
+app.use(cors());
+
+// request payLoad middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/',  (req, res, next) => {
   res.send('Hello from Node API Server');
 });
 
@@ -13,4 +21,14 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
+});
+
+// error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send({
+    status: 500,
+    message: err.message,
+    body: {}
+  });
 });
