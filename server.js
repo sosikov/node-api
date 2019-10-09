8dcd0ee8790ca0516cac1e474892e49aa57b1e4b
sosikov <n.sosikov@gmail.com>
2019-10-09 16:47:47 +0300

    create product logic

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/api/v1/product', require('./routes/productRoutes'));
+
 app.get('/',  (req, res, next) => {
   res.send('Hello from Node API Server');
 });
---
+++ service/productService.js
@@ -0,0 +1,11 @@
+const Product = require('../databasse/models/productModel');
+
+module.exports.createProduct = async (serviceData) => {
+  try {
+    let product = new Product({ ...serviceData });
+    return await product.save();
+  } catch (error) {
+    console.log(`Something went wrong: Service: createProduct`, error);
+    throw new Error(error);
+  }
+}
